5580c7bfbb6114410cadf800024617f8fae07a2d
Jelle van der Ster <jellyfish2578@gmail.com>
2021-05-17 13:25:56 +0200

    Functionality added to web app - add.py

--- executive/app.py
+++ executive/app.py
@@ -5,16 +5,18 @@ Created on Mon May 17 10:01:18 2021
 """
 
 from executive.app_info import app, db
-from executive.actions.models_f import Project
+from executive.actions.models_f import Project, Action
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
 
 @app.route('/')
 def home():
     return render_template('index.html', projects = Project.query.all())
 
-@app.route('/addproject', methods = ['GET', 'POST'])
+@app.route('/add_project', methods = ['GET', 'POST'])
 def add_project():
     if request.method == 'POST':
+        # Invalidations
         if not request.form['name']:
             flash('Please enter a valid name.', 'error')
         projects = Project.query.all()
@@ -22,7 +24,7 @@ def add_project():
             flash('This project name already exists.', 'error')
         if request.form['parent'] not in [str(x.project_id) for x in projects]:
             flash('Please enter a valid parent project name.', 'error')
-        else:
+        else: # Add project to database
             project = (Project(request.form['name'], request.form['parent']) 
                        if request.form['parent'] else Project(request.form['name'], None))
             db.session.add(project)
@@ -30,6 +32,24 @@ def add_project():
             flash('Project was successfully added')
             return redirect(url_for('home'))
     return render_template('add_project.html', projects = Project.query.all())
+
+@app.route('/add_action', methods = ['GET', 'POST'])
+def add_action():
+    if request.method == 'POST':
+        # Invalidations
+        # Same action + deadline in same project should not be allowed?
+        # Add action to database
+        deadline_date = datetime(int(request.form['deadline'][:4]),
+                                   int(request.form['deadline'][5:7]),
+                                   int(request.form['deadline'][8:10]), 
+                                   23, 59, 59.999999)
+        action = Action(request.form['name'], deadline_date,
+                        request.form['project'], False, request.form['context'])
+        db.session.add(action)
+        db.session.commit()
+        flash('Action was successfully added')
+        return redirect(url_for('home'))
+    return render_template('add_action.html', projects = Project.query.all())
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
