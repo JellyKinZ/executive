548abfabb78f8b94cf9cffd73fad3d98968cc6b7
Jelle van der Ster <jellyfish2578@gmail.com>
2021-05-17 10:55:31 +0200

    Change app_info so app uses same db as models_f

--- executive/app_info.py
+++ executive/app_info.py
@@ -2,7 +2,7 @@ from flask import Flask as f
 from flask_sqlalchemy import SQLAlchemy
 
 app = f(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///executive.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///actions/executive.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "secret key"
 
