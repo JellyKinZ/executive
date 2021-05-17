ff4fbb07284761d84880fb78a8e61de50b6ec4f2
Jelle van der Ster <jellyfish2578@gmail.com>
2021-05-17 12:53:39 +0200

    Functionality added to web app - addproject.py

--- executive/actions/models_f.py
+++ executive/actions/models_f.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from flask import Flask as f
 from flask_sqlalchemy import SQLAlchemy
 from executive.app_info import app
 
@@ -11,6 +8,10 @@ class Project(db.Model):
    name = db.Column('name', db.String(100))
    parent = db.Column(db.ForeignKey(project_id))
    
+   def __init__(self, name, parent):
+       self.name = name
+       self.parent = parent
+   
 class Action(db.Model):
     action_id = db.Column('id', db.Integer, primary_key = True)
     name = db.Column('name', db.String(100))
@@ -18,12 +19,24 @@ class Action(db.Model):
     project = db.Column('project', db.ForeignKey(Project.project_id), nullable=True)
     completed = db.Column('completed', db.Boolean, default=False)
     context = db.Column('context', db.String(100), nullable=True)
+    
+    def __init__(self, name, deadline, project, completed, context):
+       self.name = name
+       self.deadline = deadline
+       self.project = project
+       self.completed = completed
+       self.context = context
 
 class ScheduledAction(db.Model):
     scheduledaction_id = db.Column('id', db.Integer, primary_key = True)
     name = db.Column('name', db.String(100))
     cron = db.Column('cron', db.String(100))
     lastcompleted = db.Column('lastcompleted', db.DateTime, nullable=True)
+    
+    def __init__(self, name, cron, lastcompleted):
+       self.name = name
+       self.cron = cron
+       self.lastcompleted = lastcompleted
    
 if __name__ == '__main__':
     db.create_all()
--- executive/app.py
+++ executive/app.py
@@ -1,17 +1,35 @@
-# -*- coding: utf-8 -*-
 """
 Created on Mon May 17 10:01:18 2021
 
 @author: jelle
 """
 
-from executive.app_info import app
+from executive.app_info import app, db
 from executive.actions.models_f import Project
+from flask import render_template, request, redirect, url_for, flash
 
 @app.route('/')
 def home():
-    projects = Project.query.all()
-    project = projects[0]
-    return "Top project: " + project.name
+    return render_template('index.html', projects = Project.query.all())
+
+@app.route('/addproject', methods = ['GET', 'POST'])
+def add_project():
+    if request.method == 'POST':
+        if not request.form['name']:
+            flash('Please enter a valid name.', 'error')
+        projects = Project.query.all()
+        if request.form['name'] in [x.name for x in projects]:
+            flash('This project name already exists.', 'error')
+        if request.form['parent'] not in [str(x.project_id) for x in projects]:
+            flash('Please enter a valid parent project name.', 'error')
+        else:
+            project = (Project(request.form['name'], request.form['parent']) 
+                       if request.form['parent'] else Project(request.form['name'], None))
+            db.session.add(project)
+            db.session.commit()
+            flash('Project was successfully added')
+            return redirect(url_for('home'))
+    return render_template('add_project.html', projects = Project.query.all())
+
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
